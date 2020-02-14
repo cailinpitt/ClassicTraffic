2c3d24a449a8e52b3c8fe21ac09ca3fb493fa791
Cailin Pitt <cailinpitt1@gmail.com>
2020-02-14 16:05:08 -0500

    started adding cameras

---
+++ cameras.js
@@ -0,0 +1,22 @@
+export const cameras = [
+    {
+        "name": "I-71 at I-75",
+        "url": "https://itscameras.dot.state.oh.us/images/artimis/CCTV035a-L.jpg",
+        "city": "Cincinnati",
+    },
+    {
+        "name": "I-75 at 3rd St",
+        "url": "https://itscameras.dot.state.oh.us/images/artimis/CCTV022.jpg",
+        "city": "Cincinnati",
+    },
+    {
+        "name": "I-71 at Gilbert Ave",
+        "url": "https://itscameras.dot.state.oh.us/images/artimis/CCTV_I-71-Gilbert.jpg",
+        "city": "Cincinnati",
+    },
+    {
+        "name": "I-471 SB just S of Ky Rt 8",
+        "url": "http://www.trimarc.org/images/milestone/CCTV_06_471_0045.jpg",
+        "city": "Cincinnati",
+    },
+];
