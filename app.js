bbaeb311541fcb47dde5dec3e0ae6ba8d47b01e7
Cailin Pitt <cailinpitt1@gmail.com>
2020-02-15 16:12:01 -0500

    add tweet functionality

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
 const cameras = require('./cameras.js');
+const keys = require('./keys.js');
 const Path = require('path');
 const Axios = require('axios');
 const Fs = require('fs');
 const _ = require('lodash');
+const Twit = require('twit');
+
+const T = new Twit({
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token: keys.access_token,
+    access_token_secret: keys.access_token_secret,
+  });
 
 const download = async () => {
+    const chosenCamera =  _.sample(cameras);
     const path = Path.resolve(__dirname, 'camera.jpg')
     const writer = Fs.createWriteStream(path)
 
     const response = await Axios({
-        url: _.sample(cameras).url,
+        url: chosenCamera.url,
         method: 'GET',
         responseType: 'stream'
     });
 
     response.data.pipe(writer)
 
-  return new Promise((resolve, reject) => {
-    writer.on('finish', resolve)
-    writer.on('error', reject)
-  })
-};
+    writer.on('finish', () => {
 
-const tweet = async () => {
+        const file = Fs.readFileSync('./camera.jpg', { encoding: 'base64' });
 
+        T.post('media/upload', { media_data: file }, function (err, data, response) {
+            const media_id = data.media_id_string;
+            const text = chosenCamera.name + "\n\n" + new Date().toLocaleTimeString();
+            const meta_params = { media_id, alt_text: { text } }
+
+            T.post('media/metadata/create', meta_params, function (err, data, response) {
+                const params = { status: text, media_ids: [media_id] }
+                    
+                    T.post('statuses/update', params);
+            });
+        });
+    });
 };
 
 download();
