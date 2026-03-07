c4bce6b8ef7792abe1572c8118935553b9a4667c
Cailin Pitt <cailinpitt1@gmail.com>
2026-03-07 16:13:41 -0600

    Fix timeout for live video bots

--- states/westvirginia.js
+++ states/westvirginia.js
@@ -74,10 +74,10 @@ class WestVirginiaBot extends TrafficBot {
       });
     });
 
-    const encodeCmd = `ffmpeg -y -i "${tempPath}" -c:v libx264 -preset medium -crf 23 -pix_fmt yuv420p -vf "setpts=0.25*PTS" -an "${this.pathToVideo}"`;
+    const encodeCmd = `ffmpeg -y -i "${tempPath}" -c:v libx264 -preset ultrafast -crf 23 -pix_fmt yuv420p -vf "setpts=0.25*PTS" -an "${this.pathToVideo}"`;
 
     await new Promise((resolve, reject) => {
-      exec(encodeCmd, { timeout: 120000 }, (error) => {
+      exec(encodeCmd, { timeout: (duration * 2 + 300) * 1000 }, (error) => {
         if (error) return reject(error);
         resolve();
       });
